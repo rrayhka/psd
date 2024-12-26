8b88200b5ef4a05bef9cebd0999b04646f1e512a
rrayhka <khyar075@gmail.com>
2024-12-26 15:56:21 +0700

    modified app

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy import stats
 import seaborn as sns
-import pylab
 import pickle
 import warnings
 warnings.filterwarnings("ignore")
